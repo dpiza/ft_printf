3c616081c215d579d1423329badb91e3c0d3c2c7
Denny Piza <dpiza@student.42sp.org.br>
2021-09-03 20:13:50 -0300

    add condicao para ignorar flag 0 com flag -

--- src/flag_parse.c
+++ src/flag_parse.c
@@ -6,7 +6,7 @@
 /*   By: dpiza <dpiza@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/17 16:21:56 by dpiza             #+#    #+#             */
-/*   Updated: 2021/09/01 19:50:17 by dpiza            ###   ########.fr       */
+/*   Updated: 2021/09/03 20:05:20 by dpiza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,10 @@ int	is_flag(char c, t_flags *flags)
 {
 	if (c == '-')
 		flags->justify = TRUE;
-	else if (c == '0' && flags->width == FALSE && flags->precision == FALSE)
+	else if (c == '0' 
+			&& flags->width == FALSE
+			&& flags->precision == FALSE 
+			&& flags->justify == FALSE)
 		flags->zerofill = 48;
 	else if (c == '+')
 		flags->plus = TRUE;
@@ -69,13 +72,13 @@ int	is_precision(char c, t_flags *flags)
 void	init_struct(t_flags *flags)
 {
 	flags->justify = 0;
-	flags->invisible_plus = 0;
 	flags->plus = 0;
+	flags->invisible_plus = 0;
 	flags->width = 0;
-	flags->zero_x = 0;
-	flags->zerofill = 32;
 	flags->precision = 0;
 	flags->precision_n = 0;
+	flags->zerofill = 32;
+	flags->zero_x = 0;
 	flags->specifier = 0;
 	flags->params_length = 0;
 	flags->params = ft_strdup("%");
